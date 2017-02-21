51b150437b6df4a7d683692cf1da908e3b227800
Berkhman <Janya.b.5@mail.ru>
2017-02-22 01:54:00 +0300

    MSC

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,8 +1,8 @@
 /**
  * Created by Евгений on 22.02.2017.
  */
-public class FirstClass {
+public class SecondClass {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World, again.");
     }
 }
